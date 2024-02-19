47a2aa8d478d60bb19b90664b951bcb37dcd316d
Nicrozoft <121097250+Nicrozoft@users.noreply.github.com>
2024-02-19 11:17:42 +0800

    Update Files

--- Nicrozoft_i964.py
+++ Nicrozoft_i964.py
@@ -2,9 +2,10 @@ import json
 import os
 
 def getfiles(path):
+	print(1)
 	global cnt
 	try:
-		df = os.listdir(correntpath)
+		df = os.listdir(path)
 	except:
 		return []
 	ff = []
@@ -14,14 +15,16 @@ def getfiles(path):
 		cnt += 1
 	folders = [name for name in df if not os.path.isfile(os.path.join(path, name))]
 	for folder in folders:
-		ff.append(getfiles(path + '\\' + folder).insert(0, folder))
+		fd = getfiles(path + '\\' + folder)
+		fd.insert(0, folder)
+		ff.append(fd)
 	return ff
 
 def compress_file(filename, cnt):
 	os.system('makecab /l %s %s %d.i964'% (outptd, filename, cnt))
 
 def uncompress_file(filename, outptdir):
-	os.system('7z x %s -o%s'% (filename, outptdir))
+	os.system('7z x %s -o%s -aoa'% (filename, outptdir))
 
 def compress_files(curtree, path):
 	ct = curtree[1:]
@@ -30,6 +33,15 @@ def compress_files(curtree, path):
 			compress_file((path + '\\' + f_d[1]), f_d[0])
 		else:
 			compress_files(f_d, path + '\\' + f_d[0])
+
+def uncompress_files(curtree, path):
+	ct = curtree[1:]
+	for f_d in ct:
+		if str(type(f_d[0])) == "<class 'int'>":
+			uncompress_file(('%s\\%d'% (compressed_dir, f_d[0])) + '.i964', (path + '\\'))
+		else:
+			os.system('md %s\\%s'% (path, f_d[0]))
+			uncompress_files(f_d, path + '\\' + f_d[0])
 
 os.system('title Nicrozoft i964')
 
@@ -47,18 +59,32 @@ while True:
 		os.system('cls')
 		print('使用 Nicrozoft i964 封装文件夹\n正在获取文件目录结构信息……\n')
 		cnt = 0
-		tree = getfiles(originaldir).insert(0, '')
-		print(tree)
-		os.system('pause')
+		tree = getfiles(originaldir)
+		tree.insert(0, '')
 		os.system('cls')
 		print('使用 Nicrozoft i964 封装文件夹\n正在封装文件……\n')
 		os.system('md %s'% (outptd))
 		compress_files(tree, originaldir)
-		json.dump(tree, '%s\\%s'% (outptd, 'tree.json'))
+		with open('%s\\%s'% (outptd, 'tree.json'), 'w', encoding = 'utf-8') as f:
+			json.dump(tree, f)
 		os.system('cls')
 		print('使用 Nicrozoft i964 封装文件夹\n封装完成\n')
 		os.system('pause')
 	if o == 2:
-		pass
+		os.system('cls')
+		print('使用 Nicrozoft i964 解压缩已被封装的文件夹')
+		compressed_dir = input('输入已被封装的文件夹(末尾不加路径分隔符):')
+		outptd = input('输入要解压缩到的位置(末尾不加路径分隔符):')
+		os.system('cls')
+		print('使用 Nicrozoft i964 解压缩已被封装的文件夹\n正在获取文件目录结构信息……\n')
+		with open('%s\\%s'% (compressed_dir, 'tree.json'), 'r', encoding = 'utf-8') as f:
+			tree = json.load(f)
+		os.system('cls')
+		print('使用 Nicrozoft i964 解压缩已被封装的文件夹\n正在解压缩文件……\n')
+		os.system('md %s' % (outptd))
+		uncompress_files(tree, outptd)
+		os.system('cls')
+		print('使用 Nicrozoft i964 解压缩已被封装的文件夹\n解压缩完成\n')
+		os.system('pause')
 	if o == 3:
 		break
